1748700eb4a44b94fc67ed9dd4ef5194522b160b
RitikaDoi <75316079+RitikaDoi@users.noreply.github.com>
2021-01-05 21:26:49 +0530

    Add files via upload

---
+++ Rop.js
@@ -0,0 +1,45 @@
+class rop
+{
+
+    constructor(bodyA, bodyB, offsetX, offsetY)
+    {
+
+      this.offsetX = offsetX;
+      this.offsetY = offsetY;
+
+      var rop_options =
+      {
+
+        //The 2 bodies which will be constrained.
+        bodyA: bodyA,
+        bodyB: bodyB,
+
+        //Setting stiffness and length.
+        stiffness: 0.04,
+        length: 300,
+
+        pointB: {x: this.offsetX, y: this.offsetY}
+
+    }
+
+    //Creating a constrained body called body.
+    this.rop = constraint.create(rop_options);
+    World.add(world, this.rop);
+
+}
+
+    display()
+    {
+
+        //Name spacing bodyA and bodyB position. 
+        var posA = this.rop.bodyA.position;
+        var posB = this.rop.bodyB.position;
+
+        //Creating a line.
+        strokeWeight(3);
+        line(posA.x, posA.y, posB.x, posB.y);
+
+    }
+
+
+}
