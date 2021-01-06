98408a7dddac1b2a00bc676c8c4581fa74f9e1e4
RitikaDoi <75316079+RitikaDoi@users.noreply.github.com>
2021-01-06 17:50:32 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,31 @@
+class roof 
+{
+    
+    constructor() 
+    {
+      
+        var options = 
+        {
+          
+            isStatic: true
+      
+        }
+     
+        this.body = Bodies.rectangle(600, 50, 300, 20,options);
+        this.width = 300;
+        this.height = 20;
+        World.add(world, this.body);
+    
+    }
+    
+    display()
+    {
+      
+        var pos =this.body.position;
+      rectMode(CENTER);
+      fill("yellow");
+      rect(pos.x, pos.y, this.width, this.height);
+    
+    }
+  
+};
